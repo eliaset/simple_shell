701a2f79d622b2ec1a109f9da2e7557a73aa4210
Elias Shamil <eliasshamil@gmail.com>
2022-08-11 11:37:30 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -17,6 +17,10 @@
 /* Global environemnt */
 extern char **environ;
 
+/* Global program name */
+char *name;
+/* Global history counter */
+int hist;
 
 /**
  * struct list_s - A new struct type defining a linked list.
